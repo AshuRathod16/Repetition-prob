76a524099ac896668a30142006868cea1e60f21c
Ashwini Rathod <rashwini510@gmail.com>
2022-05-08 22:46:38 +0530

    Added:power of two

---
+++ Power_of_two_while.sh
@@ -0,0 +1,15 @@
+n=$1
+powerOfTwo=1
+echo "Power of two:"
+   i=1
+	while [[ i -le $n ]]
+   do
+   echo $powerOfTwo
+   powerOfTwo=$(($powerOfTwo*2))
+		if [ $powerOfTwo == 256 ]
+		then
+			echo $powerOfTwo
+			break;
+		fi
+	((i++))
+   done
